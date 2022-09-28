46f845cee9a1a15011f96a4dd881269d0ce67d40
Zach-Trani <trani.zach@gmail.com>
2022-09-27 20:50:08 -0400

    js refresher format

--- js-refresher/functions.js
+++ js-refresher/functions.js
@@ -1,25 +1,24 @@
-function greet (name){
+
+function greet(name) {
     console.log("hello " + name)
 }
-
-greet("Zach")
-greet("Zach")
 greet("Zach")
 
-function add (num1,num2){
+
+function add(num1, num2) {
     return num1 + num2;
 }
 
-console.log(add(1,2))
+console.log(add(1, 2))
 
-function subtract (num1,num2){
+function subtract(num1, num2) {
     return num1 - num2;
 }
 
-console.log(subtract(1,2))
+console.log(subtract(1, 2))
 
-function average (num1,num2,num3){
-    return (num1+num2+num3) / (3);
+function average(num1, num2, num3) {
+    return (num1 + num2 + num3) / (3);
 }
 
-console.log(average(1,2,3))
+console.log(average(1, 2, 3))
