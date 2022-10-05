1a992b32402759ed0f53e827047cf5a7250bd63a
Zach-Trani <trani.zach@gmail.com>
2022-10-04 21:18:27 -0400

    added some notes from hw asssignments

---
+++ class13-materials/js-info/variables-tasks/main.js
@@ -0,0 +1,25 @@
+// Task 1
+let admin, firstName;
+
+firstName = 'John';
+
+admin = firstName;
+
+alert(admin)
+
+
+// Task 2
+let ourPlanet = 'Earth'
+
+let currentUserName = 'John'
+
+
+// Task 3
+const BIRTHDAY = '18.04.1982'; // make birthday uppercase? - known prior to execution, uppercase ok
+
+const AGE = someCode(BIRTHDAY); // make age uppercase? - No, calculated at runtime
+
+
+
+
+
