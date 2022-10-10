22543bb313caa35b05d794987e1edd7e7e90e132
Zach-Trani <trani.zach@gmail.com>
2022-10-09 20:59:09 -0400

    javascript.info/function-basics tasks

--- class13-materials/js-info/variables-tasks/main.js
+++ class13-materials/js-info/variables-tasks/main.js
@@ -1,3 +1,5 @@
+// Part 1: Task set from https://javascript.info/variables
+
 // Task 1
 let admin, firstName;
 
@@ -20,6 +22,49 @@ const BIRTHDAY = '18.04.1982'; // make birthday uppercase? - known prior to exec
 const AGE = someCode(BIRTHDAY); // make age uppercase? - No, calculated at runtime
 
 
+// Part 2: Task set from https://javascript.info/function-basics
+
+// Notes:
+//
+// It’s a good practice to minimize the use of global variables. Modern code has few or no globals. 
+// Most variables reside in their functions. Sometimes though, they can be useful to store project-level data.
+
+// Task 1 - Will the function work differently if else is removed
+function checkAge(age) {
+    if (age > 18) {
+      return true;
+    } else {    // we can remove this and the function still executes the same
+      // ...
+      return confirm('Did parents allow you?');
+    }
+}
 
 
+// Task 2 - Rewrite it, to perform the same, but without if, in a single line.
 
+// Make two variants of checkAge:
+
+// Using a question mark operator ?
+// Using OR ||
+
+function checkAge(age) {
+    return (age > 18) ? true : confirm('Did parents allow you?');
+}
+  
+function checkAge(age) {
+    return (age > 18) || confirm('Did parents allow you?');
+}
+
+// More compact way of writing if else statements
+
+
+// Task 3
+// Write a function min(a,b) which returns the least of two numbers a and b.
+
+function min(numOne, numTwo){
+    
+}
+
+min(2, 5) == 2
+min(3, -1) == -1
+min(1, 1) == 1
